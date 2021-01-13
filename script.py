2d5a60863fce5c36749725c0748a718f02fd64b5
Shway Wang <shuwei4@ualberta.ca>
2021-01-13 08:13:53 +0800

    test

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import requests
 print(requests.__version__)
 page = requests.get('https://www.google.com')
+print(page.status_code)
+print(page.text)
 script_itself = requests.get('https://raw.githubusercontent.com/ShwayW/CMPUT404_lab1/shway/script.py')
 print(script_itself.text)
